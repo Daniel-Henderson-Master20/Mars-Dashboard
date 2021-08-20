549f77c862d91d013cd7fd361a878280d505f61e
Daniel Henderson <danielhenderson@Daniels-MacBook-Air.local>
2021-08-19 20:04:03 -0400

    Made API call from backend

--- src/public/client.js
+++ src/public/client.js
@@ -44,73 +44,12 @@ const App = (state) => {
 }
 
 // listening for load event because page should load before any JS is called
-window.addEventListener("load", () => {
-    // getRover(store, store.get("selectedRover"))
+window.addEventListener('load', () => {
     render(root, store)
 })
 
 // ------------------------------------------------------  COMPONENTS
-const Navigation = (state) => {
-        const rovers = state.get("rovers")
-        return `
-		<header>
-					<div class="tab">
-					${rovers
-						.map((rover) => {
-							return `<button class="tablinks" onClick="getRover(store, '${rover}')">${rover}</button>`
-						})
-						.join("")}
-					</div>
-				</header>
-	`
-}
 
-// RoverInfo component
-const RoverInfo = (roverInfo) => {
-	if (roverInfo.isEmpty()) {
-		return `<p>Loading...</p>`
-	}
-	return `
-	<section>
-		<img src=""/>
-	</section>
-		<section>
-      <h3>${roverInfo.get("name")}</h3>
-                <p>Landing Date: ${roverInfo.get("landing_date")}</p>
-                <p>Launch Date: ${roverInfo.get("launch_date")}</p>
-								<p>Status: ${roverInfo.get("status")}</p>
-								<p>Total Images: ${roverInfo.get("total_photos")}</p>
-            </section>
-	`
-}
-
-//Main component
-const HomePage = (state) => {
-	// const roverInfo = state.get("roverInfo")
-	return `
-		<main>
-						${Greeting(state.get("user").get("name"))}
-						${RoverInfo(state.get("roverInfo"))}      
-        </main>
-	`
-}
-
-//Page Component
-//Higher Order Function
-const Layout = (state, header, main) => {
-	return `
-		${header(state)}
-		${main(state)}
-		<footer>Built by Richard</footer>
-	`
-}
-
-// App Content
-// const App = (state) => {
-// 	return `
-// 				${Layout(state, Navigation, HomePage)}
-//     `
-// }
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
 const Greeting = (name) => {
     if (name) {
@@ -133,7 +72,7 @@ const ImageOfTheDay = (apod) => {
     console.log(photodate.getDate(), today.getDate());
 
     console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
+    if (!apod || apod.date === today.getDate()) {
         getImageOfTheDay(store)
     }
 
@@ -154,29 +93,7 @@ const ImageOfTheDay = (apod) => {
 
 // ------------------------------------------------------  API CALLS
 
-// // Example API call
-const getRover = (state, rover) => {
-	fetch(`http://localhost:3000/rover/${rover}`)
-		.then((res) => res.json())
-		.then((roverInformation) =>
-			// console.log("rover:", roverInformation.rover)
-			updateStore(state, {
-				selectedRover: rover,
-				roverInfo: Immutable.Map(roverInformation.rover),
-			})
-		)
-	getRoverImages(state, rover)
-}
-
-const getRoverImages = (state, rover) => {
-	fetch(`http://localhost:3000/rover/images/${rover}`)
-		.then((res) => res.json())
-		.then((roverImages) =>
-			updateStore(state, {
-				roverImages: Immutable.List(roverImages.photos),
-			})
-		)
-}
+// Example API call
 const getImageOfTheDay = (state) => {
     let { apod } = state
 
--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,8 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.listen(port, () => console.log(`Mars Rover Dashboard app listening on port ${port}!`))
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -25,4 +27,27 @@ app.get('/apod', async(req, res) => {
     }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/rovers/:rover', async(req, res) => {
+try {
+    const { rover } = req.params
+    let roverDetails = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?max_date&api_key=${process.env.API_KEY}`)
+        .then(res => res.json())
+    res.send({ roverDetails })
+} catch (err) {
+    console.log('error:', err);
+}
+})
+)
+
+// Rovers
+// app.get('/rovers/spirit', async(req, res) => {
+//     try {
+//         const { rover } = req.params;
+//         let image = await fetch(`
+//         https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=1000&api_key=${process.env.API_KEY}`)
+//             .then(res => res.json())
+//         res.send({ image })
+//     } catch (err) {
+//         console.log('error:', err);
+//     }
+// })
