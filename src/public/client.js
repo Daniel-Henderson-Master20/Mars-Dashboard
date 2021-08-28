8134730fe2c527a41d8faef509f67b76cf65e242
Daniel Henderson <danielhenderson@Daniels-MacBook-Air.local>
2021-08-28 12:27:56 -0400

    getting tiles to leave, need data to populate

--- src/public/client.js
+++ src/public/client.js
@@ -1,175 +1,65 @@
-// Main data storage object, uses Immutable for the rover list
 let store = {
     selectedRover: '',
     data: '',
     rovers: Immutable.List(['Spirit', 'Opportunity', 'Curiosity']),
 }
 
-// add our markup to the page
 const root = document.getElementById('root')
 
 const updateStore = (roverName, roverData) => {
-        newState = {}
-        newState[roverName] = roverData
-        store = Object.assign(store, newState)
-        render(root, store)
-    }
-    // use of async with render function
+    newState = {}
+    newState[roverName] = roverData
+    store = Object.assign(store, newState)
+    render(root, store)
+}
 
-console.log(store)
 const render = async(root, state) => {
     root.innerHTML = App(state)
 }
 
-// function that creates intro content
 const App = (state) => {
-
-        return `
+    return `
         <header><h1>Mars Rover Dashboard</h1></header>
         <main>
-            <section>
-                ${RoverData(state)}
+            <section id="section-me">
+            ${wrapInDivFunction(state, 'rover-container', joinMapperFunction,
+            state.rovers, roverCardMakerFunction)}
             </section>
         </main>
         <footer>
             <h3>A project by Daniel Henderson Developer</h3>
         </footer>
     `
-    }
-    // listening for load event, page should load before any JS is called
+}
 window.addEventListener('load', () => {
     render(root, store)
 })
 
-// ------------------------------------------------------  COMPONENTS
-
-
-const RoverData = (state) => {
-
-    // If no rover is selected in state, create the rover card HTML with 
-    // wrapInDivFunction and return
-    if (!state.selectedRover) {
-        return (`
-            ${wrapInDivFunction(state, 'rover-container', joinMapperFunction,
-            state.rovers, roverCardMakerFunction)}
-        `)
-    }
-
-    // If a rover is selected but there's no data yet, call the API with
-    // getRoverData and return
-    // getRoverData will call the updateStore function, which calls this
-    // function again, so returning immediately after prevents errors in
-    // console with undefined data
-    if (!state.data) {
-        getRoverData(state)
-        return ''
-    }
-
-    // Array of photos from the rover
-    let photos
-    if (state.selectedRover === 'Curiosity') {
-        photos = state.data.results.latest_photos
-    } else {
-        photos = state.data.results.photos
-
-    }
-
-    // Map the photo array to get the URLs of the photos
-    const photoURL = photos.map(photo => photo.img_src)
-
-    // All photos will be from the same date, so use photo[0]
-    const photoDate = photos[0].earth_date
-
-    // Get the required mission data
-    const { name, launch_date, landing_date, status } = photos[0].rover
-
-    // Makes the information HTML and calls wrapInDivFunction to start the
-    // production of the photo array
-    return (`
+const onClick = (roverName) => {
+    document.getElementById("section-me").innerHTML = (`
         <ul class="info-container">
-            <li>Rover name: ${name}</li>
-            <li>Launched from Earth on: ${launch_date}</li>
-            <li>Landed on Mars on: ${landing_date}</li>
-            <li>Mission status: ${status}</li>
-            <li>Photos taken on: ${photoDate}</li>
+        <li>Rover name: ${roverName}</li>
+        <li>Launched from Earth on: ${roverName}</li>
+        <li>Landed on Mars on: ${roverName}</li>
+        <li>Mission status: ${roverName}</li>
+        <li>Photos taken on: ${roverName}</li>
         </ul>
-        <button onclick="updateStore({selectedRover: '', data: ''})" class="back-button">Back</button>
-        ${wrapInDivFunction(state, 'photo-container', joinMapperFunction,
-        photoURL, photoElementMakerFunction)}
-        <button onclick="updateStore({selectedRover: '', data: ''})" class="back-button">Back</button>
     `)
 }
 
-// --------------- COMPONENT HELPER FUNCTIONS, INCLUDING HIGHER-ORDER FUNCTIONS
-
-// /**
-//  * @description Higher-order function to wrap elements of mapped array in a div
-//  * it is used to make containers for the rover cards and photos
-//  * @param {string} divClass Name of the class that will be assigned to the div
-//  * @param {function} mapperFunction Function that will do the mapping
-//  * @param {Array} mapThis The array that will be mapped
-//  * @param {function} elementMakerFunction Function to create the element HTML
-//  */
 const wrapInDivFunction = (state, divClass, mapperFunction, mapThis, elementMakerFunction) => {
-    return (`
-    <div class="${divClass}">
-        ${mapperFunction(state, mapThis, elementMakerFunction)}
-    </div >
-    `)
+    return (`<div class="${divClass}"> ${mapperFunction(state, mapThis, elementMakerFunction)} </div>`)
 }
 
-// /**
-// //  * @description Higher-order function to make a joined map
-// //  * In this project it is used to make arrays of rover cards and photos and join
-// //  * @param {Array} mapThis The array to be mapped and joined
-// //  * @param {function} elementMakerFunction Function to use for mapping
-// //  */
 const joinMapperFunction = (state, mapThis, elementMakerFunction) => {
-    return (`
-        ${mapThis.map(x => elementMakerFunction(state, x)).join('')}
-    `)
+    return (`${mapThis.map(x => elementMakerFunction(state, x)).join('')}`)
 }
 
-// /**
-//  * @description Makes button HTML for a rover card on main UI
-//  * @param {string} rover Name of the rover
-//  * @returns Button HTML
-//  */
 const roverCardMakerFunction = (state, rover) => {
-    return (`
-    <button class="rover-card"
-    onclick="setTimeout(updateStore, 3000, {selectedRover: '${rover}'})">
-    <h2 class="card-title">${rover}</h2>
-    </button>
-    `)
+    return (`<button class="rover-card" onclick="onClick('${rover}')"> <h2 class="card-title"> ${rover} </h2> </button>`)
 }
 
-// /**
-//  * @description Makes image tag HTML for a photo URL
-//  * @param {string} url URL of the photo
-//  * @returns Image tag HTML
-//  */
-// const photoElementMakerFunction = (data) => {
-//     const root = document.getElementById('root')
-//     root.innerHTML = `
-//     <div>
-//     <img class="${data.image.latest_photos[0].img_src}" alt="Photo taken on Mars/></div>
-//     `
-// }
-
-// photoElementMakerFunction()
-
-// ------------------------------------------------------  API CALLS
-// roverNames = store.rovers // pull list from the store
-
-// roverNames.forEach((roverName) => {
-//     fetch(`http://localhost:3000/rovers/${roverName}/photos`)
-//         .then(res => res.json())
-//         .then(data =>
-//             updateStore(data.image.latest_photos[0]))
-// })
-
-roverNames = store.rovers // pull list from the store
+roverNames = store.rovers
 
 roverNames.forEach((roverName) => {
     fetch(`http://localhost:3000/rovers/${roverName}/photos`)
--- src/server/index.js
+++ src/server/index.js
@@ -16,16 +16,7 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
 
-// example API call
-// app.get('/apod', async(req, res) => {
-//     try {
-//         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-//             .then(res => res.json())
-//         res.send({ image })
-//     } catch (err) {
-//         console.log('error:', err);
-//     }
-// })
+
 
 app.get('/rovers/:rover/photos', async(req, res) => {
     try {
